94bc9ac0b0494ffed9e4a6d128042a666ee04d95
Unknown <almabekov@gmail.com>
2018-05-20 16:05:25 -0300

    Tutorial 4 part 4 done

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -4,6 +4,34 @@ using System.Collections;
 public class Fractal : MonoBehaviour
 {
 
+    public Mesh mesh;
+    public Material material;
+    public int maxDepth;
+    private int depth;
+    public float childScale;
+    private void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        if (depth < maxDepth)
+        {
+            new GameObject("Fractal Child").
+                    AddComponent<Fractal>().Initialize(this);
+        }
+    }
+    private void Initialize (Fractal parent)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        transform.parent = parent.transform;
+        childScale = parent.childScale;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = Vector3.up * (0.5f + 0.5f * childScale);
+
+    }
 }
+
 
     
