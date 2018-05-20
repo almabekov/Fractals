f64d4a09746269bf0ee469b516b76180a7580ee9
Unknown <almabekov@gmail.com>
2018-05-20 16:45:28 -0300

    Tutorial 4 part 5 done

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -15,11 +15,24 @@ public class Fractal : MonoBehaviour
         gameObject.AddComponent<MeshRenderer>().material = material;
         if (depth < maxDepth)
         {
-            new GameObject("Fractal Child").
-                    AddComponent<Fractal>().Initialize(this);
+            StartCoroutine(CreateChildren());
+            
         }
     }
-    private void Initialize (Fractal parent)
+
+    private IEnumerator CreateChildren()
+    {
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").
+                    AddComponent<Fractal>().Initialize(this, Vector3.up, Quaternion.identity);
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.right, Quaternion.Euler(0f,0f,-90f));
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.left, Quaternion.Euler(0f,0f,90f));
+    }
+    private void Initialize (Fractal parent, Vector3 direction, Quaternion orientation)
     {
         mesh = parent.mesh;
         material = parent.material;
@@ -27,8 +40,9 @@ public class Fractal : MonoBehaviour
         depth = parent.depth + 1;
         transform.parent = parent.transform;
         childScale = parent.childScale;
+        transform.localRotation = orientation;
         transform.localScale = Vector3.one * childScale;
-        transform.localPosition = Vector3.up * (0.5f + 0.5f * childScale);
+        transform.localPosition = direction * (0.5f + 0.5f * childScale);
 
     }
 }
